37ed466d16e658edecf69ef10d2c8a0f6a8a61d6
Alison Haire <alisonhaire@Alisons-MBP.localdomain>
2021-10-05 00:13:19 +1100

    fix deploy error

--- pages/components/LatestPing.js
+++ pages/components/LatestPing.js
@@ -3,20 +3,24 @@ import { CHAIN_MAP } from "../api/utils/dataMaps";
 
 const LatestPing = ({ latestPing }) => {
   return (
-    <div key={latestPing}>
-      <Header as="h5" style={{ padding: 0, margin: 0 }}>
-        Latest Ping
-      </Header>
-      <p as="h5" style={{ padding: 0, margin: 0 }}>
-        Chain:{" "}
-        {`${CHAIN_MAP[latestPing.attributes.chain_id]}  (id: ${
-          latestPing.attributes.chain_id
-        })`}
-      </p>
-      <p as="h5" style={{ padding: 0, margin: 0 }}>
-        Sender: {latestPing.attributes.sender}
-      </p>
-    </div>
+    <>
+      {latestPing && latestPing.attributes && (
+        <div key={latestPing}>
+          <Header as="h5" style={{ padding: 0, margin: 0 }}>
+            Latest Ping
+          </Header>
+          <p as="h5" style={{ padding: 0, margin: 0 }}>
+            Chain:{" "}
+            {`${CHAIN_MAP[latestPing.attributes.chain_id]}  (id: ${
+              latestPing.attributes.chain_id
+            })`}
+          </p>
+          <p as="h5" style={{ padding: 0, margin: 0 }}>
+            Sender: {latestPing.attributes.sender}
+          </p>
+        </div>
+      )}
+    </>
   );
 };
 
