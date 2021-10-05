fdd7997adb1a6c9d95cfba29d44b006fa60ac748
Alison Haire <alisonhaire@Alisons-MBP.localdomain>
2021-10-05 16:45:08 +1100

    Added syncABI.json for easy reference

--- pages/api/contracts/syncDetails.js
+++ pages/api/contracts/syncDetails.js
@@ -1,44 +1,26 @@
-const ABI = {
-  anonymous: false,
-  inputs: [
-    {
-      indexed: true,
-      internalType: "address",
-      name: "sender",
-      type: "address",
-    },
-    {
-      indexed: false,
-      internalType: "uint256",
-      name: "chain_id",
-      type: "uint256",
-    },
-    {
-      indexed: false,
-      internalType: "uint256",
-      name: "current_count",
-      type: "uint256",
-    },
-  ],
-  name: "ChainPinged",
-  type: "event",
-};
 const topic = "ChainPinged(address, uint256, uint256)";
+import { ABI } from "./syncABI.json";
 
 const syncEvents = {
   bsc: {
     description: "Chain Pinged - BSC",
     address: "0xbc775665dbfC5a621E978f568724382467e880d1",
     tableName: "BSCPing",
+    topic,
+    ABI,
   },
   polygon: {
     description: "Chain Pinged - Polygon",
     address: "0x4c498c7d2e76de0dac253b4acd85ba127e1140ed",
     tableName: "PolygonPing",
+    topic,
+    ABI,
   },
   kovan: {
     description: "Chain Pinged - Kovan",
     address: "0x54fdf91322a1767c2e26d82555759b06f21f4a37",
-    tableName: "kovanPing",
+    tableName: "KovanPing",
+    topic,
+    ABI,
   },
 };
