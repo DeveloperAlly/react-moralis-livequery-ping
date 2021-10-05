c7f63a7e4097671ef4ee87f00cc1c4303ee2e927
Alison Haire <alisonhaire@Alisons-MBP.localdomain>
2021-10-05 21:13:10 +1100

    sync cloud function

--- pages/api/cloudFunctions/InitFunction.js
+++ pages/api/cloudFunctions/InitFunction.js
@@ -1,5 +1,7 @@
 import { Moralis } from "react-moralis";
 
+//Would be good to pipe this data into a database all together & update it when pings come in
+//Not sure if possible?
 Moralis.Cloud.define("FetchInitialData", async (request) => {
   const polyData = await new Moralis.Query("PolygonPing")
     .descending("createdAt")
